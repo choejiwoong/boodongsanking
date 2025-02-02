a86a29b5f1703d136b74f2c6dcf3d617f09f8704
choejiwoong <wldndchl0926@naver.com>
2025-02-02 23:01:13 +0900

    2025/02/02 11:00: 구조 다시 원래대로 바꿈

--- weolboo/app.py
+++ weolboo/app.py
@@ -1,16 +1,14 @@
 import streamlit as st
-from pages import home, population
+import home_page, population_page
 
 def main():
     st.sidebar.title("부동산 임장보고서 App")
     page = st.sidebar.selectbox("페이지를 선택하세요", ["홈", "연령대별 인구 분석", "분석 결과"])
 
     if page == "홈":
-        home.show_home_page()
+        home_page.show_home_page()
     elif page == "연령대별 인구 분석":
-        population.show_population_page()
-    elif page == "분석 결과":
-        analysis.show_analysis_page()
+        population_page.show_population_page()
 
 if __name__ == "__main__":
     main()
--- weolboo/pages/home.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def show_home_page():
-    st.title("부동산 임장보고서 App")
-    st.write("환영합니다! 분석 분야를 선택하세요.")
